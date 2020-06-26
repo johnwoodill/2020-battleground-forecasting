27f81a9cc5e709a219d7dcb44ed1daf728ad04f0
johnwoodill <johnwoodill@gmail.com>
2020-06-26 11:01:58 -0700

    updating fig data

--- 4-Figures.R
+++ 4-Figures.R
@@ -1,19 +1,19 @@
-library(tidyverse)
+library(ggplot2)
 
 ### Load result data
-dat = read_csv("data/model_results.csv")
+dat = read.csv("data/model_results.csv")
 
 ### Load polling data
-pdat = read_csv("data/processed_538_polling_data.csv")
+pdat = read.csv("data/processed_economist_data.csv")
 
 
 ggplot() +
-  geom_line(data = filter(dat, candidate == "Biden"), aes(date, pct, group=factor(sample)), color='blue', alpha=0.05) +
-  geom_line(data = filter(dat, candidate == "Trump"), aes(date, pct, group=factor(sample)), color='red', alpha=0.05) +
-  geom_smooth(data = filter(dat, candidate == "Trump"), aes(date, pct), color='red', size=0.5, se = FALSE) +
-  geom_smooth(data = filter(dat, candidate == "Biden"), aes(date, pct), color='blue', size=0.5, se = FALSE) +
-  geom_point(data = filter(pdat, candidate == "Biden"), aes(date, pct), color='blue', alpha=0.5) +
-  geom_point(data = filter(pdat, candidate == "Trump"), aes(date, pct), color='red', alpha=0.5) +
+  geom_line(data = subset(dat, candidate == "Biden"), aes(date, pct, group=factor(sample)), color='blue', alpha=0.05) +
+  geom_line(data = subset(dat, candidate == "Trump"), aes(date, pct, group=factor(sample)), color='red', alpha=0.05) +
+  geom_smooth(data = subset(dat, candidate == "Trump"), aes(date, pct), color='red', size=0.5, se = FALSE) +
+  geom_smooth(data = subset(dat, candidate == "Biden"), aes(date, pct), color='blue', size=0.5, se = FALSE) +
+  geom_point(data = subset(pdat, candidate == "Biden"), aes(date, pct), color='blue', alpha=0.5) +
+  geom_point(data = subset(pdat, candidate == "Trump"), aes(date, pct), color='red', alpha=0.5) +
   theme_bw() +
   theme(legend.position="none") +
   ylim(.20, .80) +
