932f3f91755833deaf7699577d0359fdce8aef74
johnwoodill <johnwoodill@gmail.com>
2020-06-26 17:57:50 -0700

    fixed loess method in figure

--- 4-Figures.R
+++ 4-Figures.R
@@ -1,24 +1,25 @@
 library(ggplot2)
 
 ### Load result data
-dat = read.csv("data/model_results.csv")
+dat = read.csv("data/model_results.csv", stringsAsFactors = False)
+dat$date = as.Date(dat$date, format='%Y-%m-%d')
 
 ### Load polling data
-pdat = read.csv("data/processed_economist_data.csv")
+pdat = read.csv("data/processed_economist_data.csv", stringsAsFactors = False)
+pdat$date = as.Date(pdat$date, format='%Y-%m-%d')
 
 
 ggplot() +
   geom_line(data = subset(dat, candidate == "Biden"), aes(date, pct, group=factor(sample)), color='blue', alpha=0.05) +
   geom_line(data = subset(dat, candidate == "Trump"), aes(date, pct, group=factor(sample)), color='red', alpha=0.05) +
-  geom_smooth(data = subset(dat, candidate == "Trump"), aes(date, pct), color='red', size=0.5, se = FALSE) +
-  geom_smooth(data = subset(dat, candidate == "Biden"), aes(date, pct), color='blue', size=0.5, se = FALSE) +
+  geom_smooth(data = subset(dat, candidate == "Trump"), aes(date, pct), color='red', size=0.5, se = FALSE, method='loess') +
+  geom_smooth(data = subset(dat, candidate == "Biden"), aes(date, pct), color='blue', size=0.5,  se = FALSE, method='loess') +
   geom_point(data = subset(pdat, candidate == "Biden"), aes(date, pct), color='blue', alpha=0.5) +
   geom_point(data = subset(pdat, candidate == "Trump"), aes(date, pct), color='red', alpha=0.5) +
   theme_bw() +
   theme(legend.position="none") +
-  ylim(.20, .80) +
   labs(x=NULL, y="Share of Vote") +
-  facet_wrap(~state, ncol = 2) +
+  facet_wrap(~state, ncol = 2, scales='free') +
   NULL
 
 
